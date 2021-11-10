18fcbaa47ba9f8e5264dbf079d502ee685db41dc
ahabhgk <ahabhgk@gmail.com>
2021-11-10 18:00:36 +0800

    chore: use rust 2021

--- cli/src/module.rs
+++ cli/src/module.rs
@@ -2,10 +2,10 @@ use crate::error::AnyError;
 use rusty_qjs::{context::JsContext, value::JsValue};
 
 pub fn js_module_set_import_meta(
-  ctx: &JsContext,
-  module: &JsValue,
-  use_realpath: bool,
-  is_main: bool,
+  _ctx: &JsContext,
+  _module: &JsValue,
+  _use_realpath: bool,
+  _is_main: bool,
 ) -> Result<(), AnyError> {
   // TODO
   Ok(())
